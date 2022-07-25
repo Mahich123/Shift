c2814818c9c812a7d34cee5f30d889ca8f6c57ea
Sijey <cjpraveen@hotmail.com>
2022-07-25 20:35:36 +0530

    Update basic.go

--- mylang/basic.go
+++ mylang/basic.go
@@ -3,21 +3,21 @@ package main
 import (
     "fmt"
     "strings"
+    "io/ioutil"
+    "os"
 )
 
 const DIGITS = "0123456789";
 const ALPABETS = "abcdefghijklmnopqrstuvwxyz";
 
-var code = "print(\"Hello, World!\")";
-
-func Lexer() int {
+func Lexer(code string) int {
 
     var tok string = "";
     var chars = strings.Split(code, "");
 
     for i := 0;i < len(chars);i++ {
         tok += chars[i];
-        
+
         // fmt.Println(tok)
 
         if tok == " " {
@@ -32,6 +32,20 @@ func Lexer() int {
     return 0;
 }
 
+func readFile(path string) string {
+    var data, err = ioutil.ReadFile(path)
+
+    if err != nil {
+        return ""
+    }
+
+    return string(data)
+}
+
 func main() {
-	fmt.Println(Lexer())
+    if len(os.Args) > 1 {
+        fmt.Println(Lexer(readFile(os.Args[1])))
+    } else {
+        fmt.Println("version: 1.0")
+    }
 }
