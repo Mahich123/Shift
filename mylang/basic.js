2cbf3390a1298fb1d81a1df14b30cf01296ce2c8
Sijey <cjpraveen@hotmail.com>
2022-07-25 12:14:46 +0530

    Update basic.js

--- mylang/basic.js
+++ mylang/basic.js
@@ -2,20 +2,25 @@ const {readFile} = require("fs")
 
 const DIGITS = "0123456789"
 const ALPABETS = "abcdefghijklmnopqrstuvwxyz"
+const DECIMAL = "0.00"
 
-function Lexer() {
+function Lexer(code) {
     return
 }
 
-function Token() {
-    return
-}
-
-function print(value) {
-    console.log(value)
+function Token(code) {
+    const tt = {
+        True : "TT_BOOLEAN",
+        False : "TT_BOOLEAN",
+        None : "TT_None",
+        def : "TT_DEF",
+        return : "TT_RETURN",
+    }
+    
+    return tt
 }
 
 readFile("src/test.txt", {encoding : "utf-8"}, (err, f) => {
-    if (f) print(f)
-    if (err) print(err)
+    if (f) console.log(f)
+    if (err) console.log(err)
 })
