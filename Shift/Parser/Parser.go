ff8bba911ea510eaa31831b733a79f1256daae73
Sijey <cjpraveen@hotmail.com>
2022-08-06 18:27:56 +0530

    Update Parser.go

--- Shift/Parser/Parser.go
+++ Shift/Parser/Parser.go
@@ -7,14 +7,17 @@ import (
 func Parser(token [] string) int {
 
     for i := 0; i < len(token); i++ {
-        if token[i] == "TT_PRINT" {
+        switch token[i] {
+        case "TT_PRINT":
             if token[i + 1] == "TT_LPAREN" && token[len(token) - 1] == "TT_RPAREN" {
                 if token[i + 2] == "TT_QUOTES" && token[len(token) - 2] == "TT_QUOTES" {
                     fmt.Println(token[i + 4])
                 } else if token[i + 2] == "TT_INT" {
                     fmt.Println(token[i + 3])
+                } else if token[i + 2] == "TT_BOOLEAN" {
+                    fmt.Println(token[i + 3])
                 }
-            }
+            }            
         }
         i += 1;
     }
