19ae08459e17bf37fc1af89e739f05292fced431
Sijey <cjpraveen@hotmail.com>
2022-08-16 18:51:24 +0530

    Add files via upload

---
+++ Shift/main.py
@@ -0,0 +1,8 @@
+# this is a comment
+
+print("Hello, World!") # this print 'Hello, World!' in the terminal.
+
+print("# this is not a comment")
+
+print(True) # boolean True
+print(False) # boolean False
