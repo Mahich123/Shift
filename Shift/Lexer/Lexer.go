d9faea3f47ce91adc72803c5ba51f3f471e5abd8
Sijey <cjpraveen@hotmail.com>
2022-08-17 19:20:19 +0530

    Update Lexer.go

--- Shift/Lexer/Lexer.go
+++ Shift/Lexer/Lexer.go
@@ -13,43 +13,58 @@ func Lexer(code string) []string {
 
     var tok string = "";
     var chars = strings.Split(code, "");
-    var types = [] string {};
+    var types = []string {};
     var __str string = "";
-    var __int string = "";
     var sst int = 0;
-    var ist int = 0;
 
     for i := 0;i < len(chars);i++ {
         tok += chars[i];
 
         switch tok {
-            case "print":
-                if sst == 0 {
-                    types = append(types, "TT_PRINT");
-                    tok = "";    
-                }
+		case "print":
+			if sst == 0 {
+				types = append(types, "TT_PRINT");
+				tok = "";    
+			}
+		case "True":
+			if sst == 0 {
+				types = append(types, "TT_BOOLEAN", "True");
+				tok = "";
+			}
+		case "False":
+			if sst == 0 {
+				types = append(types, "TT_BOOLEAN", "False");
+				tok = "";
+			}
         }
+
+		switch chars[i] {
+		case "#":
+			if sst == 0 {
+				return types;
+			}
+		case "\n":
+			if sst == 0 {
+				tok = "";
+			}
+		case "(":
+			if sst == 0 {
+				types = append(types, "TT_LPAREN");
+				tok = "";
+			}
+		case ")":
+			if sst == 0 {
+				types = append(types, "TT_RPAREN");
+				tok = "";
+			}
+		}
 
         if chars[i] == " " {
             if sst == 1 {
-                __str += chars[i]
+                __str += chars[i];
             } else {
                 tok  = "";
             }
-        } else if chars[i] == "\n" && sst == 0 {
-            tok = "";
-        } else if chars[i] == "#" && sst == 0 {
-            return types
-        } else if chars[i] == "(" && sst == 0 {
-            types = append(types, "TT_LPAREN");
-            tok = "";
-        } else if chars[i] == ")" && sst == 0 {
-            if ist == 1 {
-                types = append(types, "TT_INT", __int);
-                ist = 0
-            }
-            types = append(types, "TT_RPAREN");
-            tok = "";
         } else if chars[i] == "\"" {
             if sst == 0 {
                 sst = 1;
@@ -62,18 +77,6 @@ func Lexer(code string) []string {
             tok = "";
         } else if sst == 1 {
             __str += chars[i];
-            tok = "";
-        } else if sst == 0 && strings.ContainsAny(DIGITS, tok) {
-            if strings.ContainsAny(ALPABETS, tok) {
-                return types
-            }
-            ist = 1
-            __int += chars[i];
-        } else if sst == 0 && tok == "True" {
-            types = append(types, "TT_BOOLEAN", "True")
-            tok = "";
-        } else if sst == 0 && tok == "False" {
-            types = append(types, "TT_BOOLEAN", "False")
             tok = "";
         }
     }
