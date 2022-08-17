73bf8c13c76da7ff198e810b8038cf4ef66c6fba
Sijey <cjpraveen@hotmail.com>
2022-08-17 19:40:28 +0530

    Update main.go

--- Shift/main.go
+++ Shift/main.go
@@ -9,23 +9,20 @@ import (
     "Shift/Parser"
 )
 
-func readFile(path string) string {
-    var data, err = ioutil.ReadFile(path);
-
-    if err != nil {
-        return "";
-    }
-
-    return string(data);
-}
-
 func main() {
     if len(os.Args) > 1 {
-        var lines = strings.Split(readFile(os.Args[1]), "\n");
-        for i := 0; i < len(lines); i++ {
+		var data, err = ioutil.ReadFile(os.Args[1]);
+
+		if err != nil {
+			fmt.Println("");
+		}
+
+		var lines = strings.Split(string(data), "\n");
+
+		for i := 0; i < len(lines); i++ {
             Parser.Parser(Lexer.Lexer(lines[i]));
         }
     } else {
-        fmt.Println("version: 1.0");
+        fmt.Println("Shift version: 1.0a (https://github.com/sijey-praveen/Shift)");
     }
 }
